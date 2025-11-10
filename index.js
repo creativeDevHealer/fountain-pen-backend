e7421dbca65543c6760788da4d64f04fb399b9fb
creativeDevHealer <code.healer.1120@gmail.com>
2025-11-09 16:38:12 -0800

    scrape.do token changed

--- index.js
+++ index.js
@@ -181,19 +181,19 @@ var kleinanzeigenScrapeConfig = {
 
 var ebayScrapeConfig = {
     method: 'get',
-    url: 'http://api.scrape.do/?url=https%3A%2F%2Fwww.ebay.co.uk%2Fsch%2Fi.html%3F_stpos%3DSK62QN%26_svsrch%3D1%26_nkw%3D%E2%80%9Cmontblanc%E2%80%9D%2Band%2B%E2%80%9C149%E2%80%9D%26LH_TitleDesc%3D1%26_sop%3D10%26LH_PrefLoc%3D2%26_fcid%3D3%26_folent%3D42101235225%26mkpid%3D0%26emsid%3De11021.m164379.l178259%26mkcid%3D7%26ch%3Dosgood%26euid%3D4467d5740f154c16996dc9a56fcd53bc%26bu%3D43600934330%26ut%3DRU%26exe%3D0%26ext%3D0%26osub%3D-1%7E1%26crd%3D20251008033812%26segname%3D11021%26_ipg%3D240%26rt%3Dnc%26LH_ItemCondition%3D3&token=b940fda662f8430f9e3d74e0c73491ac2d770959eea',
+    url: 'http://api.scrape.do/?url=https%3A%2F%2Fwww.ebay.co.uk%2Fsch%2Fi.html%3F_stpos%3DSK62QN%26_svsrch%3D1%26_nkw%3D%E2%80%9Cmontblanc%E2%80%9D%2Band%2B%E2%80%9C149%E2%80%9D%26LH_TitleDesc%3D1%26_sop%3D10%26LH_PrefLoc%3D2%26_fcid%3D3%26_folent%3D42101235225%26mkpid%3D0%26emsid%3De11021.m164379.l178259%26mkcid%3D7%26ch%3Dosgood%26euid%3D4467d5740f154c16996dc9a56fcd53bc%26bu%3D43600934330%26ut%3DRU%26exe%3D0%26ext%3D0%26osub%3D-1%7E1%26crd%3D20251008033812%26segname%3D11021%26_ipg%3D240%26rt%3Dnc%26LH_ItemCondition%3D3&token=26bc757fb6954f4f8160ad98b279a6c459141117319',
     headers: {
     },
 };
 var lotArtScrapeConfig = {
     method: 'get',
-    url: 'http://api.scrape.do/?url=https%3A%2F%2Fwww.lot-art.com%2Fauction-search%3Fis_ss%3D1%26q%3D%22montblanc%22+and+%22149%22%26section%3D1%26slo%3D0%26cat%3D%26subcat%3D%26houses%3D%26houses_excl%3D0%26country%3D%26min_price%3D%26max_price%3D%26min_deal_pcnt%3Dnull%26max_deal_pcnt%3Dnull%26currency%3D%26item_type%3D0%26min_create_time%3D0%26order%3Drecent%26context_subcat_only%3D%26context_label3%3D%26context_is_attributed%3D%26context_is_copy%3D%26page%3D1%26res_per_page%3D20%26subq%3D%26context_name_brand%3D%26context_name_brand_birth_death%3D&token=b940fda662f8430f9e3d74e0c73491ac2d770959eea',
+    url: 'http://api.scrape.do/?url=https%3A%2F%2Fwww.lot-art.com%2Fauction-search%3Fis_ss%3D1%26q%3D%22montblanc%22+and+%22149%22%26section%3D1%26slo%3D0%26cat%3D%26subcat%3D%26houses%3D%26houses_excl%3D0%26country%3D%26min_price%3D%26max_price%3D%26min_deal_pcnt%3Dnull%26max_deal_pcnt%3Dnull%26currency%3D%26item_type%3D0%26min_create_time%3D0%26order%3Drecent%26context_subcat_only%3D%26context_label3%3D%26context_is_attributed%3D%26context_is_copy%3D%26page%3D1%26res_per_page%3D20%26subq%3D%26context_name_brand%3D%26context_name_brand_birth_death%3D&token=26bc757fb6954f4f8160ad98b279a6c459141117319',
     headers: {
     },
 };
 var carousellScrapeConfig = {
     method: 'get',
-    url: 'http://api.scrape.do/?url=https%3A%2F%2Fwww.carousell.sg%2Fsearch%2F%22montblanc%22+and+%22149%22%3FaddRecent%3Dtrue%26brand_enum%3DBRAND_MONT_BLANC%26canChangeKeyword%3Dtrue%26includeSuggestions%3Dtrue%26sort_by%3D3%26t-search_query_source%3Ddirect_search&token=b940fda662f8430f9e3d74e0c73491ac2d770959eea&super=true&geoCode=SG',
+    url: 'http://api.scrape.do/?url=https%3A%2F%2Fwww.carousell.sg%2Fsearch%2F%22montblanc%22+and+%22149%22%3FaddRecent%3Dtrue%26brand_enum%3DBRAND_MONT_BLANC%26canChangeKeyword%3Dtrue%26includeSuggestions%3Dtrue%26sort_by%3D3%26t-search_query_source%3Ddirect_search&token=26bc757fb6954f4f8160ad98b279a6c459141117319&super=true&geoCode=SG',
     headers: {
     },
 };
